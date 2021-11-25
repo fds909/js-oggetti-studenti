515632b520c3c50a8ecf2240567dad09cbe3cce4
fds909 <francescod95@gmail.com>
2021-11-25 16:51:28 +0100

    fatto pseudocodice

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* Creazione Oggetto studente (nome, cognome, età) */
+
+/* Output proprietà oggetto con for-in */
+
+/* Creazione array studenti */
+
+/* Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
+
+/* 3 prompt() per aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+
+
+
+
