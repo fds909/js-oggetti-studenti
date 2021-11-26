dd35f0a45a9032218803d116467473752db67ada
fds909 <francescod95@gmail.com>
2021-11-26 17:58:41 +0100

    push delle 18

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-/* Creazione Oggetto studente (nome, cognome, età) */
-const studente = {nome:"Fiat", cognome:"500", età:"white"};
+const studente = {nome:"Fabio", cognome:"Fazio", età:"66"};
 
-/* Output proprietà oggetto con for-in */
+for (nome in studente) {
+    console.log();
+}
 
-/* Creazione array studenti */
+const studenti = ["Mauro", "Patrizia", "Gianfranco"];
 
 /* Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
+for (let i = 0; i < studenti.length; i++) {
+    console.log(studenti[i]);
+  }
 
 /* 3 prompt() per aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
